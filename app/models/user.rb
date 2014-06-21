6e2027f124a641796c43806b69090946437803af
Zach <zholdren20@gmail.com>
2014-06-21 17:04:58 -0400

    Fixed unsupported method in rails 4

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+                  :first_name, :last_name, :profile_name
+  # attr_accessible :title, :body
 end
